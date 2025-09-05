17e47fbfb0485541b90ff3ab57db7740f13259b7
muhammad sandi <muhammadsandimaulana07@gmail.com>
2025-09-05 21:09:19 +0700

    feat(cors):add config cors

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,8 @@ dotenv.config()
 
 const config = {
   PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
+  WHITELIST_ORIGINS: ["https://"],
 }
 
 export default config
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,24 @@ import cors from "cors"
 
 import config from "@/config"
 
-import type { CorsOptions } from "cors"
 const app = express()
+import type { CorsOptions } from "cors"
 
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (
+      config.NODE_ENV === "development" ||
+      !origin ||
+      config.WHITELIST_ORIGINS.includes(origin)
+    ) {
+      callback(null, true)
+    } else {
+      callback(new Error(`CORS error: ${origin} is not allowed`))
+    }
+    console.log(`CORS error: ${origin} is not allowed by CORS`)
+  },
+}
+app.use(cors(corsOptions))
 app.get("/", (req, res) => {
   res.json({ message: "Hello world" })
 })
